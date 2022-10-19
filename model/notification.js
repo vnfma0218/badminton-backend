a1dfce63ec548c241426dd194bda31731d94b5a3
이푸름 <vnfma0218@naver.com>
2022-10-20 00:03:02 +0900

    feat: 알람 스키마 생성

---
+++ model/notification.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+const notificationSchema = mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+
+  title: {
+    type: String,
+    required: true,
+  },
+
+  created_at: {
+    type: Date,
+    default: moment().format('YYYY-MM-DD hh:mm:ss'),
+  },
+});
+const Notification = mongoose.model('Notification', notificationSchema);
+module.exports = notificationSchema;
